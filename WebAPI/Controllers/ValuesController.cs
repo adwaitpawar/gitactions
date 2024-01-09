65b8dae2287a5f29c1a46dbd5b3dcf9231ea3b4b
adwaitpawar <adwaitpawar@gmail.com>
2024-01-09 16:44:23 +0530

    first commit

---
+++ WebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace WebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+
+        public ActionResult<IEnumerable<string>> Get()
+
+        {
+
+         List<string> items = new List<string>
+
+        {
+
+            "Phone",
+
+            "Laptop",
+
+            "Camera"
+
+        };
+
+            return items;
+        }
+
+    }
+}
